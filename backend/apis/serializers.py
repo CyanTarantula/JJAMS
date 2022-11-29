1e30a52d1f758b65f185fbc65988aec3fa4e28b1
Soumik-Roy <roy.9@iitj.ac.in>
2022-11-29 10:49:17 +0530

    added student account creation api

--- backend/apis/serializers.py
+++ backend/apis/serializers.py
@@ -14,11 +14,11 @@ class StudentSerializers(serializers.ModelSerializer):
         """
         Create and return a new `student_db` instance, given the validated data.
         """
-        print(validated_data)
         try:
             hostel_fk = hostel_db.objects.get(hostel_name=validated_data['hostel_name']['hostel_name'])
             validated_data['hostel_name'] = hostel_fk
-            return student_db.objects.create(**validated_data)
+            student_instance = student_db.objects.create(**validated_data)
+            return student_instance
         except:
             raise serializers.ValidationError("Hostel does not exist")
 
@@ -26,6 +26,19 @@ class PasswordSerializers(serializers.ModelSerializer):
     class Meta:
         model = password_db
         fields = '__all__'
+    
+    def create(self, validated_data):
+        """
+        Create and return a new `student_db` instance, given the validated data.
+        """
+        print(validated_data)
+        try:
+            roll_no_fk = student_db.objects.get(roll_no=validated_data['roll_no'])
+            validated_data['roll_no'] = roll_no_fk
+            pswd_instance = password_db.objects.create(**validated_data)
+            return pswd_instance
+        except:
+            raise serializers.ValidationError("Student Instance does not exist")
 
 class WardenSerializers(serializers.ModelSerializer):
     class Meta:
--- backend/trials.py
+++ backend/trials.py
@@ -2,20 +2,34 @@ import requests
 
 # api post request
 url = "http://127.0.0.1:8000/api/students/"
+
 data = {
-    'roll_no' : 'B20AI050',
-    'student_name' : 'Yash Bhargava',
+    'roll_no' : 'B20CS029',
+    'student_name' : 'Tanmay Kulkarni',
     'programme' : 'B.Tech',
-    'branch' : 'Artificial Intelligence',
+    'branch' : 'Computer Science',
     'semester' : '5',
     'contact_no' : '+919292929292',
-    'email_id' : 'bhargava.x@iitj.ac.in',
+    'email_id' : 'kulkarni.x@iitj.ac.in',
     'guardian_contact' : '+919876543210',
     'age' : '20',
     'permanent_addr' : 'IIT Jodhpur, Rajasthan, India',
-    'room_no' : '1',
+    'room_no' : '168',
     'hostel_name' : 'G5',
 }
+
+
+r = requests.post(url = url, data = data)
+print(r.status_code)
+print(r.text)
+
+url = "http://127.0.0.1:8000/api/login/password/"
+
+data = {
+    'roll_no' : 'B20CS029',
+    'password' : '123456',
+}
+
 
 r = requests.post(url = url, data = data)
 print(r.status_code)
